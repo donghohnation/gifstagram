e18d842cdd707534bbd6f0f49b4d5932dc20e0a9
donghohnation <donghoh.han@gmail.com>
2022-08-24 22:51:32 -0700

    updated formatting, js, toggle light mode

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,42 @@
+
+
+
+document.getElementById("toggle").addEventListener("click", lightmode);
+
+function lightmode() {
+	let body = document.body;
+	body.classList.toggle("lightmode");
+};
+
+
+
+
+
+
+
+
 let APIKEY = "k97KrpZv1g8ZfdGW7o9ZVsQphqFCguwz";
 
 
 document.addEventListener("DOMContentLoaded", init);
 
 
+const cards = document.getElementsByClassName("card");
+
+for (var i = 0; i < cards.length; i++) {
+	cards[i].style.visibility = "hidden";
+}
+
+
 function init() {
 	document.getElementById("btnSearch").addEventListener("click", ev => {
 		ev.preventDefault(); //to stop the page reload
+
+		if (document.querySelector("#search").value !== "") {
+			for (var i = 0; i < cards.length; i++) {
+				cards[i].style.visibility = "visible";
+			}
+		}
 		let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=9&q=`;
 		let str = document.getElementById("search").value.trim();
 		url = url.concat(str);
@@ -28,3 +58,4 @@ function init() {
 			});
 	});
 }
+
